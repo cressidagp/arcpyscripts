59bfa2048d914a44f8529e76c23488fe9e1c880a
cressidagp <gcpigni@gmail.com>
2023-05-09 17:25:11 -0300

    Added prototype of onReachWP function to TF script.

--- boss_terenas.py
+++ boss_terenas.py
@@ -1,25 +1,86 @@
 import arcemu
 import ArcPyMath as Math
 from arcemu import Unit
+from arcemu import GossipMenu
 
-TM_STATE = {}
-TM_TIMER = {}
+TF_STATE = {}
+TF_TIMER = {}
 
-CREATUREID_TERENAS_MENETHIL = 36823
+EMOTE_STATE_READY2H = 375
+EMOTE_ONESHOT_POINT_NOSHEATHE = 397
 
-def TerenasMenethil_DoAction():
-    print("todo")   
+CREATUREID_TIRION_FORDRING = 38995
 
-def TerenasMenethil_onDamageTaken( unit, event, attacker, amount ):
+def TirionFordring_onReachWP( unit, event, wpid, foward ):
+     if wpid == 1:
+          unit.emote( EMOTE_STATE_READY2H, 0 )
+
+def TirionFordring_DoAction():
     print("todo")
+       
+def TirionFordring_onAIUpdate( unit, event ):
+    state = TF_STATE[ unit.getGUID() ]
+    timer = TF_TIMER[ unit.getGUID() ]
+    
+    if state == 0:
+        unit.sendChatMessage( arcemu.CHAT_MSG_MONSTER_SAY, arcemu.LANG_UNIVERSAL, "We'll grant you a swift death, Arthas. More than can be said for the thousands you've tortured and slain." )
+        unit.playSoundToSet( 17390 )
+        TF_TIMER[ unit.getGUID() ] = 2
+        
+    elif state == 1 and timer <= 0:
+        unit.sendChatMessage( arcemu.CHAT_MSG_MONSTER_SAY, arcemu.LANG_UNIVERSAL, "test2" )
+        unit.emote( EMOTE_ONESHOT_POINT_NOSHEATHE, 0 )
+        TF_TIMER[ unit.getGUID() ] = 3
+        
+    elif state == 3 and timer <= 0:
+        #walk
+        #move to
+        unit.sendChatMessage( arcemu.CHAT_MSG_MONSTER_SAY, arcemu.LANG_UNIVERSAL, "test3" )
+        TF_TIMER[ unit.getGUID() ] = 20
+        
+        
+    if state == 99:
+            state = 0
+    else:
+            state = state + 1
+            
+    TF_STATE[ unit.getGUID() ] = state
+    
+    timer = timer - 1
+    
+    TF_TIMER[ unit.getGUID() ] = timer
+           
+def TirionFordring_onHello( unit, event, player ):
+	
+	creature = unit.toCreature()
+	
+	menu = GossipMenu( 15290, unit, arcemu.GOSSIP_AUTOSEND_FALSE )
+	
+	menu.addItem( arcemu.ICON_CHAT, "We are prepared, Highlord. Let us battle for the fate of Azeroth! For the light of dawn!", 1, 0 )
+	menu.sendToPlayer( player )
+    
+def TirionFordring_onSelectOption( unit, player, id, enteredCode ):
 
-def TerenasMenethil_onAIUpdate( unit, event ):
-    state = TM_STATE[ unit.getGUID() ]
-    print("todo")
+    GossipMenu.complete( player )
+    unit.RegisterAIUpdateEvent( 1000 )    
+    creature = unit.toCreature()
+	
+    if id == 1:
+            creature.removeNpcFlag( arcemu.NPC_FLAG_GOSSIP )
+            creature.setMovementType( arcemu.MOVEMENTTYPE_FORWARDTHENSTOP )
 
-def TerenasMenethil_onLoad( unit, event ):
-    unit.RegisterAIUpdateEvent( 1000 )
+def TirionFordring_onLoad( unit, event ):
+    TF_STATE[ unit.getGUID() ] = 0
+    TF_TIMER[ unit.getGUID() ] = 0
+    #unit.RegisterAIUpdateEvent( 1000 )
+    creature = unit.toCreature()
+    creature.setMovementType( arcemu.MOVEMENTTYPE_DONTMOVEWP )
+    creature.resetWaypoint()
+    creature.destroyCustomWaypoints()
+    creature.createCustomWaypoint( 489.2970, -2124.840, 840.8569, 0.0, 250, arcemu.WAYPOINT_FLAG_WALK, 0 )  
 
-arcemu.RegisterUnitEvent( CREATUREID_TERENAS_MENETHIL, arcemu.CREATURE_EVENT_ON_DAMAGE_TAKEN, TerenasMenethil_onDamageTaken )
-arcemu.RegisterUnitEvent( CREATUREID_TERENAS_MENETHIL, arcemu.CREATURE_EVENT_ON_AIUPDATE, TerenasMenethil_onAIUpdate )
-arcemu.RegisterUnitEvent( CREATUREID_TERENAS_MENETHIL, arcemu.CREATURE_EVENT_ON_LOAD, TerenasMenethil_onLoad )
+arcemu.RegisterUnitGossipEvent( CREATUREID_TIRION_FORDRING, arcemu.GOSSIP_EVENT_HELLO, TirionFordring_onHello )
+arcemu.RegisterUnitGossipEvent( CREATUREID_TIRION_FORDRING, arcemu.GOSSIP_EVENT_SELECT, TirionFordring_onSelectOption )
+arcemu.RegisterUnitEvent( CREATUREID_TIRION_FORDRING, arcemu.CREATURE_EVENT_ON_REACH_WP, TirionFordring_onReachWP )
+arcemu.RegisterUnitEvent( CREATUREID_TIRION_FORDRING, arcemu.CREATURE_EVENT_ON_AIUPDATE, TirionFordring_onAIUpdate )
+arcemu.RegisterUnitEvent( CREATUREID_TIRION_FORDRING, arcemu.CREATURE_EVENT_ON_LOAD, TirionFordring_onLoad )
