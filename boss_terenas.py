1b28d171b222c7955ffd0c7973c1db376a77b455
cressidagp <gcpigni@gmail.com>
2023-05-09 12:46:00 -0300

    Splited script in three parts so it can be more easy to work on.

---
+++ boss_terenas.py
@@ -0,0 +1,25 @@
+import arcemu
+import ArcPyMath as Math
+from arcemu import Unit
+
+TM_STATE = {}
+TM_TIMER = {}
+
+CREATUREID_TERENAS_MENETHIL = 36823
+
+def TerenasMenethil_DoAction():
+    print("todo")   
+
+def TerenasMenethil_onDamageTaken( unit, event, attacker, amount ):
+    print("todo")
+
+def TerenasMenethil_onAIUpdate( unit, event ):
+    state = TM_STATE[ unit.getGUID() ]
+    print("todo")
+
+def TerenasMenethil_onLoad( unit, event ):
+    unit.RegisterAIUpdateEvent( 1000 )
+
+arcemu.RegisterUnitEvent( CREATUREID_TERENAS_MENETHIL, arcemu.CREATURE_EVENT_ON_DAMAGE_TAKEN, TerenasMenethil_onDamageTaken )
+arcemu.RegisterUnitEvent( CREATUREID_TERENAS_MENETHIL, arcemu.CREATURE_EVENT_ON_AIUPDATE, TerenasMenethil_onAIUpdate )
+arcemu.RegisterUnitEvent( CREATUREID_TERENAS_MENETHIL, arcemu.CREATURE_EVENT_ON_LOAD, TerenasMenethil_onLoad )
