0adef1d00a312d3ba5105ea3291d651934309c06
cressidagp <gcpigni@gmail.com>
2023-07-08 13:45:17 -0300

    Created script to handle spell 'Boar Charge' since its not working properly in ai_agents.

--- sql/arcpyscripts.sql
+++ sql/arcpyscripts.sql
@@ -1,3 +1,7 @@
 -- AI agent 'Rushing Charge':
 DELETE FROM `ai_agents` WHERE `spell` = 6268 AND `entry` 
 IN ( 3122, 3123, 3227 );
+
+-- AI agent 'Boar Charge':
+DELETE FROM `ai_agents` WHERE `spell` = 3385 AND `entry` 
+IN ( 3099, 3100, 3225 );
