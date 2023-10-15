341079b28a3cbad29e20adbc278ab71664844af9
cressidagp <gcpigni@gmail.com>
2023-10-15 00:12:56 -0300

    Randomly spotted bug on 'Fandral Staghelm' script.

--- pythonscripts/fandral_staghelm.py
+++ pythonscripts/fandral_staghelm.py
@@ -3,6 +3,7 @@
 Engine: APE
 Zone: Teldrassil
 Creature: Arch Druid Fandral Staghelm
+.npc portto 28680
 
 '''
 
@@ -15,16 +16,19 @@ def FandralStaghelm_onHello( unit, event, player ):
 
     menu = GossipMenu( 2285, unit, arcemu.GOSSIP_AUTOSEND_FALSE )
 
-    menu.addItem( arcemu.ICON_CHAT, "I'm not a journeyman herbalist -- am I able to still assist you in your work?", 1, 0 )
+    menu.addItem( arcemu.ICON_CHAT, "I'm not a journeyman herbalist -- am I able to still assist you in your work?", 0, 0 )
+    
     menu.addQuests( unit, player )
 
     menu.sendToPlayer( player )
 
 def FandralStaghelm_onSelectOption( unit, player, id, enteredCode ):
 
-    menu = GossipMenu( 2320, unit, arcemu.GOSSIP_AUTOSEND_FALSE )
+    if id == 0:
 
-    menu.sendToPlayer( player )
+        menu = GossipMenu( 2320, unit, arcemu.GOSSIP_AUTOSEND_FALSE )
+
+        menu.sendToPlayer( player )
 
 arcemu.RegisterUnitGossipEvent( NPC_ID_FANDRAL_STAGHELM, arcemu.GOSSIP_EVENT_HELLO, FandralStaghelm_onHello )
 arcemu.RegisterUnitGossipEvent( NPC_ID_FANDRAL_STAGHELM, arcemu.GOSSIP_EVENT_SELECT, FandralStaghelm_onSelectOption )
